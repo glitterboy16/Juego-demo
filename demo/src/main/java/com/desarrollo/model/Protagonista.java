ec015bee4c06a252af219bb783e1b2edb82ca74d
mtcalvop01 <mtcalvop01@iesalbarregas.es>
2025-04-24 17:19:46 +0200

    cambiosenenemigo

--- demo/src/main/java/com/desarrollo/model/Protagonista.java
+++ demo/src/main/java/com/desarrollo/model/Protagonista.java
@@ -2,6 +2,11 @@ package com.desarrollo.model;
 
 public class Protagonista extends Personaje {
     
+
+    public void setPersonaje(int salud, int fuerza, int defensa, int velocidad, int posicionX, int posicionY) {
+        this.personaje = new Personaje(salud, fuerza, defensa, velocidad, posicionX, posicionY );
+        notifyObservers();
+    }
     public void desplazarse(){}
 
     public void introducirEstadistica(){}
