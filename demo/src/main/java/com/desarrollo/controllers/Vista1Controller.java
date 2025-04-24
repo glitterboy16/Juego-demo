cba259144d174712b59b962713dd27589c2940ff
arubior22 <anarubio21@gmail.com>
2025-04-24 21:08:57 +0200

    cambios vista1

--- demo/src/main/java/com/desarrollo/controllers/Vista1Controller.java
+++ demo/src/main/java/com/desarrollo/controllers/Vista1Controller.java
@@ -4,10 +4,20 @@ import com.desarrollo.SceneID;
 import com.desarrollo.SceneManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.Pane;
 
 
 public class Vista1Controller {
 
+    @FXML
+    private Pane fondo;
    
     @FXML
     private Button inicio;
@@ -15,6 +25,21 @@ public class Vista1Controller {
 
     @FXML
     public void initialize() {
+        // Cargar la imagen
+        Image imagen = new Image(getClass().getResource("/imagenes/fondo1.jpg").toExternalForm());
+
+        // Configurar la imagen de fondo
+        BackgroundImage bgImage = new BackgroundImage(
+            imagen,
+            BackgroundRepeat.NO_REPEAT,
+            BackgroundRepeat.NO_REPEAT,
+            BackgroundPosition.DEFAULT,
+            new BackgroundSize(100, 100, true, true, true, false)
+        );
+
+        fondo.setBackground(new Background(bgImage));
+        
+
         inicio.setOnAction(event -> {
             SceneManager.getInstance().loadScene(SceneID.SECONDARY);
         });
--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.desarrollo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     opens com.desarrollo to javafx.fxml;
     opens com.desarrollo.controllers to javafx.fxml;
