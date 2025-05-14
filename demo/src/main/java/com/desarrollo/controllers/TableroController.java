14ce627cac45376980bbde57602db0c0224218a5
Angel <villorinaangelandres@gmail.com>
2025-05-14 14:14:53 +0200

    por fin

--- demo/src/main/java/com/desarrollo/controllers/TableroController.java
+++ demo/src/main/java/com/desarrollo/controllers/TableroController.java
@@ -304,8 +304,8 @@ public class TableroController implements Observer {
         // Agregar enemigos al tablero
         agregarEnemigo(13, 1, "/com/desarrollo/imagenes/Enemigo1_abajo.png", 15, 25, "Enemigo 1", 25, 8, 5, 6, 1);
         agregarEnemigo(1, 13, "/com/desarrollo/imagenes/Enemigo2_abajo.png", 25, 35, "Enemigo 2", 50, 7, 4, 5, 2);
-        agregarEnemigo(7, 6, "/com/desarrollo/imagenes/Enemigo3_abajo.png", 10, 15, "Enemigo 3", 25, 9, 6, 7, 3);
-        agregarEnemigo(13, 13, "/com/desarrollo/imagenes/Enemigo4_abajo.png", 30, 20, "Enemigo 4", 100, 10, 5, 8, 4);
+        agregarEnemigo(7, 6, "/com/desarrollo/imagenes/Enemigo3_abajo.png", 10, 15, "Enemigo 3", 75, 9, 6, 7, 3);
+        agregarEnemigo(13, 13, "/com/desarrollo/imagenes/Enemigo4_abajo.png", 30, 20, "Enemigo 4", 39, 10, 5, 8, 4);
         
         inicializarEstadisticas();
     }
@@ -443,6 +443,44 @@ public void onChange() {
                 E4fuerza.setText(String.valueOf(e.getFuerza()));
                 E4defensa.setText(String.valueOf(e.getDefensa()));
                 E4velocidad.setText(String.valueOf(e.getVelocidad()));
+                break;
+        }
+    }
+
+    // Limpiar etiquetas de enemigos eliminados
+    for (int i = enemigos.size(); i < 4; i++) {
+        switch (i) {
+            case 0:
+                E1nombre.setText("");
+                E1saludBar.setProgress(0);
+                E1salud.setText("");
+                E1fuerza.setText("");
+                E1defensa.setText("");
+                E1velocidad.setText("");
+                break;
+            case 1:
+                E2nombre.setText("");
+                E2saludBar.setProgress(0);
+                E2salud.setText("");
+                E2fuerza.setText("");
+                E2defensa.setText("");
+                E2velocidad.setText("");
+                break;
+            case 2:
+                E3nombre.setText("");
+                E3saludBar.setProgress(0);
+                E3salud.setText("");
+                E3fuerza.setText("");
+                E3defensa.setText("");
+                E3velocidad.setText("");
+                break;
+            case 3:
+                E4nombre.setText("");
+                E4saludBar.setProgress(0);
+                E4salud.setText("");
+                E4fuerza.setText("");
+                E4defensa.setText("");
+                E4velocidad.setText("");
                 break;
         }
     }
